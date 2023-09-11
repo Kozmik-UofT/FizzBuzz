5238acd4183538e423477ec8fec4a2c12de05adc
KozmiK64 <sjzhu.72@gmail.com>
2023-09-11 18:39:53 -0400

    added this code

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+        int num_multiples = 0;
+        while (i < 1000) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if (divisibleBy3 || divisibleBy5) {
+                num_multiples++;
+            }
+            i++;
+        }
+        System.out.println(num_multiples);
+    }
 }
